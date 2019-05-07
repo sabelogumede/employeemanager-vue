ae68d3c9c5b44655d02a2ae9f4987a37bc828eab
sabelogumede <iamsabelogumede@gmail.com>
2019-05-07 13:24:27 +0200

    connected to our firebase db, displayed our list styled with material and font-awesome

--- .../src/components/Dashboard.vue
+++ .../src/components/Dashboard.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="dashboard">
-    <h3>Dashboard</h3>
-
+    <!-- <h3>Dashboard</h3> -->
+    <ul class="collection with-header">
+      <li class="collection-header">
+        <h4>Employees</h4>
+      </li>
+      <!-- our list -->
+      <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
+        <div class="chip">{{employee.dept}}</div>
+        {{employee.employee_id}}:{{employee.name}}
+        <!-- <span>{{employee.dept}}</span> -->
+        <!-- <span>{{employee.position}}</span> -->
+        <router-link class="secondary-content" v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+          <i class="fa fa-eye"></i>
+        </router-link>
+      </li>
+    </ul>
 
     <div class="fixed-action-btn">
       <router-link to="/new" class="btn-floating btn-large red">
@@ -13,10 +27,30 @@
 </template>
 
 <script>
+import db from './firebaseinit'
 export default {
   name: "dashboard",
   data() {
-    return {};
+    return {
+      employees: []
+    }
+  },
+  created () {
+    // db.collection('employees').get().then(querySnapshot => {
+    db.collection('employees').orderBy('employee_id').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        //console.log(doc.data());
+        const data = {
+          'id': doc.id,
+          'employee_id': doc.data().employee_id,
+          'name': doc.data().name,
+          'dept': doc.data().dept,
+          'position': doc.data().position,
+        }
+        // push data onto our "empty employees array"
+        this.employees.push(data)
+      })
+    })
   }
 };
 </script>
