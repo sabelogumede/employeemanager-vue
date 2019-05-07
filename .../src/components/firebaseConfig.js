4cc2257403988d97da8f72c812b734535460bd79
sabelogumede <iamsabelogumede@gmail.com>
2019-05-07 10:58:02 +0200

    initial project file

---
+++ .../src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyDYJf953a641gzuPR9_QLGA6b0pO9Cw1HU",
+  authDomain: "vue-pwa-b9dd3.firebaseapp.com",
+  databaseURL: "https://vue-pwa-b9dd3.firebaseio.com",
+  projectId: "vue-pwa-b9dd3",
+  storageBucket: "vue-pwa-b9dd3.appspot.com",
+  messagingSenderId: "209499757682",
+  appId: "1:209499757682:web:da0816370cc52243"
+
+}
