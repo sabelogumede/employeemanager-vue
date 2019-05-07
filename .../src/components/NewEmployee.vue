57de554622bac00049aa30d49cebbe5070a04655
sabelogumede <iamsabelogumede@gmail.com>
2019-05-07 12:18:36 +0200

    added UI support 'matterial, Jquery and font-awesome

---
+++ .../src/components/NewEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="new-employee">
+        <h3>Employee</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'new-employee',
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
